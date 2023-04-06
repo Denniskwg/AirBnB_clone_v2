59ceaa894afee8030e990e6ea2d17a944d4febfa
Denniskwg <dkamau476@gmail.com>
2023-04-06 17:38:56 +0300

    corrections

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-file to practice use of Fabric
+file to practice use of Fabric and tar command
+to create an archive
 """
 from fabric.api import *
 import time
 
 
 def do_pack():
+    """task that creates an archive using tar
+    and will be ran by fabric
+    """
     timestr = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
