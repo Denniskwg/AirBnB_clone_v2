4ffe9f8a3fbc1cfd0a00dc2179e9cc938f41af5f
Denniskwg <dkamau476@gmail.com>
2023-04-24 16:22:18 +0300

    corrections

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,11 +8,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """lists all State objects in storage"""
-    state_dict = storage.all(State)
+    state_dict = storage.all("State")
     return render_template('7-states_list.html', states=state_dict)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    storage.close()
 
 
 if __name__ == "__main__":
