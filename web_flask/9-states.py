c191cbc86c235445f568e98bb2fe83057fc54c16
Denniskwg <dkamau476@gmail.com>
2023-04-24 21:43:06 +0000

    adding solution for question 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""9-states starts a Flask web application
+application must be listening on 0.0.0.0, port 5000
+Routes:
+        /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template, escape
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """lists all States in storage"""
+    state_dict = storage.all(State)
+    return render_template('9-states.html', states=state_dict)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    """lists all cities of a State in storage"""
+    states = storage.all(State)
+    state = {}
+    state_id = escape(id)
+    for stat in states.values():
+        if stat.id == state_id:
+            state['1'] = stat
+    return render_template('9-states.html', states=state)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """close db session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
